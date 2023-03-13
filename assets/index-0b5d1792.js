1902f81266126563cf3dc1a3315021f3e4304b1a
rowend36 <rowendduke36@gmail.com>
2023-03-13 07:59:29 +0100

    fixed broken deploy

--- assets/index-0b5d1792.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-58b33864.js";const r={};function t(c,n){return" About Me "}const _=e(r,[["render",t]]);export{_ as default};
---
+++ assets/index-616a62f0.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-dd512db3.js";const r={};function t(c,n){return" About Me "}const _=e(r,[["render",t]]);export{_ as default};
